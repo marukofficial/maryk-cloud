3b7fd700b7a875aebd6148d3dec53295ed6010ee
marukofficial <meditation00909@gmail.com>
2025-07-31 09:23:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-// index.js
-console.log("MaryK Cloud is ready.");
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("🚀 MaryK Cloud API connected to Shopify at:", process.env.SHOP_URL);
